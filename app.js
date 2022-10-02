db4db40660b95b08adcd6ae1772957f941d576c1
Shivani Soni <eng.shivanisoni2405@gmail.com>
2022-10-02 18:29:59 +0530

    update

--- app.js
+++ app.js
@@ -2,17 +2,17 @@ var input=document.querySelector('#input');
 var buttonIncrease=document.querySelector('#increase');
 var buttonDecrease=document.querySelector('#decrease');
 var output=document.querySelector('#output');
-// var size=16;
+var size=16;
 
 function increaseSize()
 {
-   var textIncrease=document.getElementById('input').style.fontSize='2px';
+   var textIncrease=document.getElementById('input').style.fontSize=size+'2px';
     output.innerText=textIncrease;
 
 }
 function decreaseSize()
 {
-    var textDecrease=document.getElementById('input').style.fontSize='2px';
+    var textDecrease=document.getElementById('input').style.fontSize=size-'2px';
     output.innerText=textDecrease;
     // font--;
 
